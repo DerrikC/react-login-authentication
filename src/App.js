b749bb9a734b10049e874047cfa3a89d082d527e
Derrik Campbell <derrikcampbell@hotmail.com>
2022-01-08 13:45:51 -0500

    added react bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
 
 export default function App() {
   return (
     <div>
-      Hello World
+    <Routes>
+      <Route>
+        <Route path="/" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+      </Route>
+    </Routes>
     </div>
   )
 }
