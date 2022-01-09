9eb97607f35339c6b297aaca629f54b27ffc6f95
Derrik Campbell <derrikcampbell@hotmail.com>
2022-01-09 13:32:11 -0500

    setting up signup component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,33 @@
-import React from "react"
-import { Form, Button, Card } from "react-bootstrap"
-import { Link } from "react-router-dom"
+
+import React, { useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { Link, useNavigate } from "react-router-dom";
+import { useUserAuth } from "../context/UserAuthContext";
 
 
 export default function Signup () {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+    //remove passref
+  const [password, setPassword, setPasswordRef ] = useState("");
+  const [loading, setLoading] = useState(false)
+  const { signUp } = useUserAuth();
+  let navigate = useNavigate();
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signUp(email, password);
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+    //remove
+    setLoading(false)  
+
+  };
 
     return (
         //need the blank tag for jsx formatting
@@ -11,19 +35,26 @@ export default function Signup () {
         <Card>
         <Card.Body>
         <h2 className="text-center mb-4"> Sign Up</h2>
-    
+        {error && <Alert variant="danger">{error}</Alert>}
         <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" ref={emailRef} require />
+                <Form.Control type="email"
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)} />
             </Form.Group>
             <Form.Group id="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} require />
+                <Form.Control type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)} />
             </Form.Group>
+            {/* remove password confirm group */}
             <Form.Group id="password-confirm">
                 <Form.Label>Password Confirmation</Form.Label>
-                <Form.Control type="password" ref={passwordConfirmRef} require />
+                <Form.Control type="password"
+              placeholder="Password"
+              onChange={(e) => setPasswordRef(e.target.value)} />
             </Form.Group>
             <Button disabled={loading} type="submit">Sign Up</Button>
         </Form>
--- src/context/UserAuthContext.js
+++ src/context/UserAuthContext.js
@@ -1,13 +1,13 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
+    onAuthStateChanged,
     signOut,
   } from "firebase/auth";
+import { auth } from "../firebase";
 
 const userAuthContext = createContext()
-
-const userAuthContext = createContext();
 
 export function UserAuthContextProvider({ children }) {
   const [user, setUser] = useState({});
@@ -20,4 +20,28 @@ export function UserAuthContextProvider({ children }) {
   }
   function logOut() {
     return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
+      console.log("Auth", currentuser);
+      setUser(currentuser);
+    });
+
+    return () => {
+        unsubscribe();
+      };
+    }, []);
+
+    return (
+        <userAuthContext.Provider
+          value={{ user, logIn, signUp, logOut }}
+        >
+          {children}
+        </userAuthContext.Provider>
+      );
+    }
+
+  export function useUserAuth() {
+    return useContext(userAuthContext);
   }
