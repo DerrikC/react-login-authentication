7b9c681bcf0aea0b5be868faed5cc11b5fd95f98
Derrik Campbell <derrikcampbell@hotmail.com>
2022-01-09 13:47:58 -0500

    setup login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,31 @@
-import React from "react"
-import { Form, Button, Card } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Card, Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { useUserAuth } from "../context/UserAuthContext";
 
 export default function Login() {
  
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false)
+    const { logIn } = useUserAuth();
+    const navigate = useNavigate();
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError("");
+      try {
+        await logIn(email, password);
+        navigate("/home");
+      } catch (err) {
+        setError(err.message);
+      }
+        //remove
+    setLoading(false)  
+    };
+
     return (
         <>
         <Card>
@@ -13,11 +35,15 @@ export default function Login() {
         <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" ref={emailRef} require />
+                <Form.Control type="email"
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)} />
             </Form.Group>
             <Form.Group id="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} require />
+                <Form.Control type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)} />
             </Form.Group>
             <Button disabled={loading} type="submit">Login</Button>
         </Form>
